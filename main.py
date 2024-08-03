db081733dfea2a202e75750cb76c94dbaad20dab
Utkarsh <utkjaiswal58@gmail.com>
2024-08-03 11:31:40 +0530

    port number added

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ config = {
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'host': os.getenv('DB_HOST'),
-    'database': os.getenv('DB_NAME')
+    'database': os.getenv('DB_NAME'),
+    'port':os.getenv("DB_PORT")
 }
 
 def get_tables():
